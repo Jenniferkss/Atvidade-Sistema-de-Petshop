480acb6b4bfd73ce347661789d32324a808266a4
Jennifer Ketelin <jennifer.k.silva7@aluno.senai.br>
2025-10-06 10:35:15 -0300

    feat: cria banco de dados petShop_db e tabela pets

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+
+// Criar aplicação com Express e configurar para aceitar JSON
+const app = express();
+app.use(express.json());
+
+// Carregar variáveis de ambiente e definir constante para porta do servidor
+dotenv.config();
+const serverPort = process.env.PORT || 3001;
+
+// Rota principal GET para "/"
+app.get("/", (req, res) => {
+  res.json({ message: "🐶 API dos Pets funcionando!" });
+});
+
+
+// Iniciar servidor escutando na porta definida
+app.listen(serverPort, () => {
+  console.log(`🐕 Servidor rodando em http://localhost:${serverPort} 🐶`);
+});
